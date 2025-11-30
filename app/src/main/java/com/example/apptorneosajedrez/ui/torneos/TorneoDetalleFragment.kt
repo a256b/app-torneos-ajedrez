c5e686890a7bf186b4e5c291a2cbdfeeb2ff6186
a256b <arielb02965@gmail.com>
2025-11-29 23:42:41 -0300

    fix

--- app/src/main/java/com/example/apptorneosajedrez/ui/torneos/TorneoDetalleFragment.kt
+++ app/src/main/java/com/example/apptorneosajedrez/ui/torneos/TorneoDetalleFragment.kt
@@ -49,7 +49,6 @@ class TorneoDetalleFragment : Fragment() {
                 binding.tvCantidadInscriptos.text = "Inscriptos: $cantidad"
             }
         }
-    }
 
         binding.btnVerPartidas.setOnClickListener {
             val intent = Intent(requireContext(), MovesActivity::class.java)
